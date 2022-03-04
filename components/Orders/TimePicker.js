5a69358a2303fe4156afde15b8f38f7a306cf64f
Sophie KD <sophie.dyrlund@gmail.com>
2022-03-04 07:53:45 +0100

    corrections erreurs OrderRecapScreen

--- components/Orders/TimePicker.js
+++ components/Orders/TimePicker.js
@@ -1,11 +1,54 @@
 import React, { useState } from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import { StyleSheet, View, ScrollView, Platform } from "react-native";
 import { Button, ButtonGroup, withTheme, Text } from "react-native-elements";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 // Sophie : à Faire
 
 function TimePicker() {
-  return <View>Time Picker</View>;
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
+  return (
+    <View>
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
