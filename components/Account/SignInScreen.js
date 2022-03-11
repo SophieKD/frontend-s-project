01892edf7aaa2a859476e26398a9a445c1aab75d
Sophie KD <sophie.dyrlund@gmail.com>
2022-03-11 15:23:58 +0100

    keyboard sur signin signup

--- components/Account/SignInScreen.js
+++ components/Account/SignInScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
 import { Button, Input } from "react-native-elements";
 import { FontAwesome } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 function SignInScreen(props) {
   const [email, setEmail] = useState("");
@@ -25,124 +26,126 @@ function SignInScreen(props) {
   };
 
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <View style={{ backgroundColor: "#136979", height: 50 }} />
-      <View
-        style={{
-          flex: 1,
-          flexWrap: "wrap",
-          flexDirection: "row",
-          justifyContent: "center",
-          marginTop: "10%",
-        }}
-      >
-        <Image source={require("../../assets/logo-s-2.png")} />
-      </View>
+    <KeyboardAwareScrollView>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ backgroundColor: "#136979", height: 50 }} />
+        <View
+          style={{
+            flex: 1,
+            flexWrap: "wrap",
+            flexDirection: "row",
+            justifyContent: "center",
+            marginTop: "10%",
+          }}
+        >
+          <Image source={require("../../assets/logo-s-2.png")} />
+        </View>
 
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontWeight: "bold",
-            color: "#136979",
-            fontSize: 20,
+        <View style={styles.container}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              color: "#136979",
+              fontSize: 20,
 
-            marginLeft: "5%",
-            marginRight: "5%",
-          }}
-        >
-          Connectez-vous
-        </Text>
-        <Text
-          style={{
-            fontSize: 17,
-            marginTop: "5%",
-            marginBottom: "12%",
-          }}
-        >
-          Connectez-vous et retrouvez tous vos avantages !
-        </Text>
-        <Input
-          label={"Email"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="email"
-          onChangeText={(val) => onChangeEmail(val)}
-          value={email}
-          errorMessage={props.error.email}
-        />
-        <Input
-          label={"Password"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="password"
-          secureTextEntry={hidePassword}
-          onChangeText={(val) => setPassword(val)}
-          value={password}
-          rightIcon={
-            <FontAwesome
-              name="eye"
-              size={24}
-              color="grey"
-              onPress={() => changeSecureTextEntry()}
-            />
-          }
-          errorMessage={props.error.password}
-        />
-        <Button
-          buttonStyle={{
-            marginTop: 10,
-            backgroundColor: "#136979",
-            borderRadius: 30,
-          }}
-          title="S'identifier"
-          type="solid"
-          containerStyle={{
-            width: "60%",
-            marginHorizontal: 50,
-            marginVertical: 10,
-          }}
-          titleStyle={{
-            color: "white",
-            fontSize: 20,
-            fontWeight: "bold",
-          }}
-          onPress={() => onPressSignIn(email, password)}
-        />
-        <Text
-          //Pour la suite, mécanique de récupération de password: onPress={() => goToPasswordForbidden()}
-          style={{
-            marginTop: 5,
-            marginBottom: 15,
-            textDecorationLine: "underline",
-          }}
-        >
-          Mot de passe oublié
-        </Text>
-        <Text
-          style={{
-            marginTop: "15%",
-            fontWeight: "bold",
-            color: "#136979",
-            fontSize: 17,
-          }}
-        >
-          Pas de compte ?
-        </Text>
-        <Text
-          onPress={() => goToSignUp()}
-          style={{
-            fontWeight: "bold",
-            marginTop: 10,
-            textDecorationLine: "underline",
-            fontSize: 17,
-          }}
-        >
-          Créez-en un !
-        </Text>
-      </View>
-    </ScrollView>
+              marginLeft: "5%",
+              marginRight: "5%",
+            }}
+          >
+            Connectez-vous
+          </Text>
+          <Text
+            style={{
+              fontSize: 17,
+              marginTop: "5%",
+              marginBottom: "12%",
+            }}
+          >
+            Connectez-vous et retrouvez tous vos avantages !
+          </Text>
+          <Input
+            label={"Email"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="email"
+            onChangeText={(val) => onChangeEmail(val)}
+            value={email}
+            errorMessage={props.error.email}
+          />
+          <Input
+            label={"Password"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="password"
+            secureTextEntry={hidePassword}
+            onChangeText={(val) => setPassword(val)}
+            value={password}
+            rightIcon={
+              <FontAwesome
+                name="eye"
+                size={24}
+                color="grey"
+                onPress={() => changeSecureTextEntry()}
+              />
+            }
+            errorMessage={props.error.password}
+          />
+          <Button
+            buttonStyle={{
+              marginTop: 10,
+              backgroundColor: "#136979",
+              borderRadius: 30,
+            }}
+            title="S'identifier"
+            type="solid"
+            containerStyle={{
+              width: "60%",
+              marginHorizontal: 50,
+              marginVertical: 10,
+            }}
+            titleStyle={{
+              color: "white",
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+            onPress={() => onPressSignIn(email, password)}
+          />
+          <Text
+            //Pour la suite, mécanique de récupération de password: onPress={() => goToPasswordForbidden()}
+            style={{
+              marginTop: 5,
+              marginBottom: 15,
+              textDecorationLine: "underline",
+            }}
+          >
+            Mot de passe oublié
+          </Text>
+          <Text
+            style={{
+              marginTop: "15%",
+              fontWeight: "bold",
+              color: "#136979",
+              fontSize: 17,
+            }}
+          >
+            Pas de compte ?
+          </Text>
+          <Text
+            onPress={() => goToSignUp()}
+            style={{
+              fontWeight: "bold",
+              marginTop: 10,
+              textDecorationLine: "underline",
+              fontSize: 17,
+            }}
+          >
+            Créez-en un !
+          </Text>
+        </View>
+      </ScrollView>
+    </KeyboardAwareScrollView>
   );
 }
 
--- components/Account/SignUpScreen.js
+++ components/Account/SignUpScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
 import { Button, Input } from "react-native-elements";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 function SignUpScreen(props) {
   const [firstname, setFirstname] = useState("");
@@ -38,159 +39,168 @@ function SignUpScreen(props) {
   };
 
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <View style={{ backgroundColor: "#136979", height: 50 }} />
-      <View
-        style={{
-          flex: 1,
-          flexWrap: "wrap",
-          flexDirection: "row",
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Image source={require("../../assets/logo-s-2.png")} />
-      </View>
-
-      <View style={styles.container}>
-        <Text
+    <KeyboardAwareScrollView>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ backgroundColor: "#136979", height: 50 }} />
+        <View
           style={{
-            fontWeight: "bold",
-            fontSize: 20,
-            color: "#136979",
-          }}
-        >
-          Créez un compte
-        </Text>
-        <Text
-          style={{
-            fontSize: 17,
+            flex: 1,
+            flexWrap: "wrap",
+            flexDirection: "row",
+            justifyContent: "center",
             marginTop: "5%",
-            marginBottom: "5%",
-            marginLeft: "5%",
-            marginRight: "5%",
           }}
         >
-          Créez votre compte et profitez de vos avantages !
-        </Text>
-        <Input
-          inputContainerStyle={{ height: 35 }}
-          label={"Prénom"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{
-            marginBottom: 0,
-            width: "70%",
-          }}
-          inputStyle={{
-            marginLeft: 10,
-          }}
-          placeholder="prénom"
-          onChangeText={(val) => setFirstname(val)}
-          value={firstname}
-          errorMessage={props.error.firstname}
-        />
-        <Input
-          inputContainerStyle={{ height: 35 }}
-          label={"Nom"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="nom"
-          onChangeText={(val) => setLastname(val)}
-          value={lastname}
-          errorMessage={props.error.lastname}
-        />
-        <Input
-          inputContainerStyle={{ height: 35 }}
-          label={"Pseudo"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="pseudo"
-          onChangeText={(val) => setPseudo(val)}
-          value={pseudo}
-          errorMessage={props.error.pseudo}
-        />
-        <Input
-          inputContainerStyle={{ height: 35 }}
-          label={"Mobile"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="mobile"
-          onChangeText={(val) => setMobile(val)}
-          value={mobile}
-          errorMessage={props.error.mobile}
-        />
-        <Input
-          inputContainerStyle={{ height: 35 }}
-          label={"Email"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="email"
-          onChangeText={(val) => onChangeEmail(val)}
-          value={email}
-          errorMessage={props.error.email}
-        />
-        <Input
-          inputContainerStyle={{ height: 35 }}
-          label={"Password"}
-          labelStyle={{ color: "#136979", fontSize: 15 }}
-          containerStyle={{ marginBottom: 0, width: "70%" }}
-          inputStyle={{ marginLeft: 10 }}
-          placeholder="password"
-          secureTextEntry={true}
-          onChangeText={(val) => setPassword(val)}
-          value={password}
-          errorMessage={props.error.password}
-        />
-        <Button
-          buttonStyle={{
-            marginTop: 0,
-            backgroundColor: "#136979",
-            borderRadius: 30,
-          }}
-          title="Créer son compte"
-          type="solid"
-          containerStyle={{
-            width: "60%",
-            marginHorizontal: 50,
-            marginVertical: 10,
-          }}
-          titleStyle={{
-            color: "white",
-            fontSize: 20,
-            fontWeight: "bold",
-          }}
-          onPress={() =>
-            onPressSignUp(firstname, lastname, pseudo, mobile, email, password)
-          }
-        />
-        <Text
-          style={{
-            marginTop: "8%",
-            fontWeight: "bold",
-            color: "#136979",
-            fontSize: 17,
-          }}
-        >
-          Vous avez déjà un compte ?
-        </Text>
-        <Text
-          onPress={() => goToSignIn()}
-          style={{
-            fontWeight: "bold",
-            marginTop: 10,
-            textDecorationLine: "underline",
-            fontSize: 17,
-          }}
-        >
-          Cliquez ici pour vous connecter !
-        </Text>
-        <View style={{ height: 50 }}></View>
-      </View>
-    </ScrollView>
+          <Image source={require("../../assets/logo-s-2.png")} />
+        </View>
+
+        <View style={styles.container}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 20,
+              color: "#136979",
+            }}
+          >
+            Créez un compte
+          </Text>
+          <Text
+            style={{
+              fontSize: 17,
+              marginTop: "5%",
+              marginBottom: "5%",
+              marginLeft: "5%",
+              marginRight: "5%",
+            }}
+          >
+            Créez votre compte et profitez de vos avantages !
+          </Text>
+          <Input
+            inputContainerStyle={{ height: 35 }}
+            label={"Prénom"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{
+              marginBottom: 0,
+              width: "70%",
+            }}
+            inputStyle={{
+              marginLeft: 10,
+            }}
+            placeholder="prénom"
+            onChangeText={(val) => setFirstname(val)}
+            value={firstname}
+            errorMessage={props.error.firstname}
+          />
+          <Input
+            inputContainerStyle={{ height: 35 }}
+            label={"Nom"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="nom"
+            onChangeText={(val) => setLastname(val)}
+            value={lastname}
+            errorMessage={props.error.lastname}
+          />
+          <Input
+            inputContainerStyle={{ height: 35 }}
+            label={"Pseudo"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="pseudo"
+            onChangeText={(val) => setPseudo(val)}
+            value={pseudo}
+            errorMessage={props.error.pseudo}
+          />
+          <Input
+            inputContainerStyle={{ height: 35 }}
+            label={"Mobile"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="mobile"
+            onChangeText={(val) => setMobile(val)}
+            value={mobile}
+            errorMessage={props.error.mobile}
+          />
+          <Input
+            inputContainerStyle={{ height: 35 }}
+            label={"Email"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="email"
+            onChangeText={(val) => onChangeEmail(val)}
+            value={email}
+            errorMessage={props.error.email}
+          />
+          <Input
+            inputContainerStyle={{ height: 35 }}
+            label={"Password"}
+            labelStyle={{ color: "#136979", fontSize: 15 }}
+            containerStyle={{ marginBottom: 0, width: "70%" }}
+            inputStyle={{ marginLeft: 10 }}
+            placeholder="password"
+            secureTextEntry={true}
+            onChangeText={(val) => setPassword(val)}
+            value={password}
+            errorMessage={props.error.password}
+          />
+          <Button
+            buttonStyle={{
+              marginTop: 0,
+              backgroundColor: "#136979",
+              borderRadius: 30,
+            }}
+            title="Créer son compte"
+            type="solid"
+            containerStyle={{
+              width: "60%",
+              marginHorizontal: 50,
+              marginVertical: 10,
+            }}
+            titleStyle={{
+              color: "white",
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+            onPress={() =>
+              onPressSignUp(
+                firstname,
+                lastname,
+                pseudo,
+                mobile,
+                email,
+                password
+              )
+            }
+          />
+          <Text
+            style={{
+              marginTop: "8%",
+              fontWeight: "bold",
+              color: "#136979",
+              fontSize: 17,
+            }}
+          >
+            Vous avez déjà un compte ?
+          </Text>
+          <Text
+            onPress={() => goToSignIn()}
+            style={{
+              fontWeight: "bold",
+              marginTop: 10,
+              textDecorationLine: "underline",
+              fontSize: 17,
+            }}
+          >
+            Cliquez ici pour vous connecter !
+          </Text>
+          <View style={{ height: 50 }}></View>
+        </View>
+      </ScrollView>
+    </KeyboardAwareScrollView>
   );
 }
 
